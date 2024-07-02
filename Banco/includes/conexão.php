a783a399a2c46ec464287e961f153203c9ab75db
clarateodoro <clara.teodoro@aluno.ifsp.edu.br>
2024-07-02 10:26:34 -0300

    criabanco

---
+++ Banco/includes/conexão.php
@@ -0,0 +1,11 @@
+<?php
+$hostname ='localhost';
+$username = 'root';
+$password = '';
+$port     = '3307';
+
+$con =  mysqli_connect($hostname, $username, $password, $database, $port);
+if(mysqli_connect_errno()){
+    printf("Erro conexão: %s", mysqli_connect_error());
+    exit();
+}
